86e83b2909c41ab88b6bb76650eac6ea08630e89
izanmubarak <izan@umich.edu>
2017-12-22 18:06:17 -0500

    Put token in Updater field

--- Cryptora.py
+++ Cryptora.py
@@ -499,7 +499,7 @@ def error(bot, update, error):
 
 def main():
     # Create the Updater and pass it your bot's token.
-    updater = Updater(token)
+    updater = Updater('488131377:AAGqibrO_D6nHsTnvNGzsBtPogoojpXK2eU')
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
